e5d3c3eed07a2bcb66f639c7f366329840ccf4fc
Shazee17 <shahzaman856.com>
2023-05-06 09:49:47 +0500

    updated CSS

--- src/Components/TodoContainer.jsx
+++ src/Components/TodoContainer.jsx
@@ -1,39 +1,42 @@
-import React, {useState} from 'react'
-import TodoForm from './TodoForm'
-import { v4 as uuidv4 } from 'uuid';
-import Todo from './Todo';
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
 uuidv4();
 
 const TodoContainer = () => {
-
   const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => {
-    setTodos([...todos, {
-      id: uuidv4(),
-      task: todo
-    }]);
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        task: todo,
+      },
+    ]);
     console.log(todos);
-  }
+  };
 
-  const deleteTodo = id => {
-    setTodos(todos.filter(todo => todo.id !== id)) 
-  }
-
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+    
   return (
-    <div className='container'>
-        <h1> TO <span>DO</span></h1>
-        <TodoForm addTodo={addTodo}/>
+    <div className="container">
+      <h1>
+        {" "}
+        TO <span>DO</span>
+      </h1>
+      <TodoForm className="todo-form" addTodo={addTodo} />
 
+      <div className="todo-container">
         {todos.map((todo, index) => (
-
-            <Todo task={todo} key={index}
-            onDelete={() => deleteTodo(todo.id)} />
-
+          <Todo task={todo} key={index} onDelete={() => deleteTodo(todo.id)} />
         ))}
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TodoContainer
+export default TodoContainer;
